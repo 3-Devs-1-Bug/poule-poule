7d2883d4f76b18c3749cd5da7ad01823c0ae390b
Quentin Bellanger <hello@quentin-bellanger.com>
2019-11-27 00:48:58 +0100

    Apply testing library pattern to get element

--- client/src/components/Header/Header.test.tsx
+++ client/src/components/Header/Header.test.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, getByText } from '@testing-library/react'
 import Header from './Header'
 
 describe('Header', () => {
   it('renders without crashing', () => {
-    const { container } = render(<Header />)
+    const { container } = render(<Header title='title' subtitle='subtitle' />)
     expect(container.firstElementChild).toBeInTheDocument()
   })
 
   it('renders a Header', () => {
-    const { container } = render(<Header />)
+    const { container } = render(<Header title='title' subtitle='subtitle' />)
     expect(container.firstElementChild!.tagName).toBe('HEADER')
   })
 
   it('renders the title prop', () => {
-    const { container } = render(<Header title='Hello world' />)
-    const title = container.querySelector('h1')
-    expect(title).toHaveTextContent('Hello world')
+    const { container } = render(<Header title='title' subtitle='subtitle' />)
+    const title = getByText(container, 'title')
+    expect(title).toHaveTextContent('title')
   })
 
   it('renders the subtitle prop', () => {
-    const { container } = render(<Header subtitle='dlrow olleH' />)
-    const title = container.querySelector('span')
-    expect(title).toHaveTextContent('dlrow olleH')
+    const { container } = render(<Header title='title' subtitle='subtitle' />)
+    const subtitle = getByText(container, 'subtitle')
+    expect(subtitle).toHaveTextContent('subtitle')
   })
 })
