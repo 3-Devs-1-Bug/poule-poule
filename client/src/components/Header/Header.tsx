ad7a55ef5b4d71785db8b2c6cca9b51ab2af1f0d
Quentin Bellanger <hello@quentin-bellanger.com>
2019-11-27 00:41:52 +0100

    Add className prop

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import classnames from 'classnames'
 
 import icon from '../../images/icon.svg'
 import './Header.scss'
@@ -6,16 +7,21 @@ import './Header.scss'
 export interface HeaderProps {
   title: string
   subtitle: string
+  className?: string
 }
 
-const Header: FC<HeaderProps> = ({ title, subtitle }) => (
-  <header className='Header'>
-    <img className='Header__Icon' src={icon} alt='' />
-    <div>
-      <h1>{title}</h1>
-      <span className='Header__Subtitle'>{subtitle}</span>
-    </div>
-  </header>
-)
+const Header: FC<HeaderProps> = ({ title, subtitle, className }) => {
+  const classes = classnames(className, 'Header')
+
+  return (
+    <header className={classes}>
+      <img className='Header__Icon' src={icon} alt='' />
+      <div>
+        <h1>{title}</h1>
+        <span className='Header__Subtitle'>{subtitle}</span>
+      </div>
+    </header>
+  )
+}
 
 export default Header
