29ee60b597d8542730d19ae6f6ea67bd7daa3e3f
ThunderDev1 <sgranger1@gmail.com>
2019-12-11 23:16:16 +0100

    PR rework

--- client/src/pages/Lobby/Lobby.tsx
+++ client/src/pages/Lobby/Lobby.tsx
@@ -2,13 +2,15 @@ import React, { FC, useEffect, useState } from 'react'
 import axios, { AxiosResponse } from 'axios'
 import moment from 'moment'
 import { RouteComponentProps } from '@reach/router'
+import { HubConnection } from '@microsoft/signalr'
+
 import { Settings as SettingsType } from '../../types/Settings'
 import connectToGameHub from '../../utils/signalrConnector'
 import { Game } from '../../types/Game'
 import Settings from '../../containers/Settings'
 import './Lobby.scss'
 import PlayerBox from '../../components/PlayerBox'
-import { HubConnection } from '@microsoft/signalr'
+import { GameStatus } from '../../types/GameStatus'
 
 export interface LobbyProps extends RouteComponentProps {
   id?: string
@@ -30,7 +32,7 @@ const Lobby: FC<LobbyProps> = (props: LobbyProps) => {
       var game = await loadGame()
       setGame(game)
 
-      if (game.status === 'PENDING_START') {
+      if (game.status === GameStatus.PENDING_START) {
         const connection = connectToGameHub(game.id)
         connection.start().then(() => {
           connection.connectionId && setCurrentPlayerId(connection.connectionId)
@@ -69,18 +71,20 @@ const Lobby: FC<LobbyProps> = (props: LobbyProps) => {
           ) : (
             <ul>
               <li>
-                Niveau de difficulté: <b>{game.difficulty}</b>
+                Niveau de difficulté: <strong>{game.difficulty}</strong>
               </li>
               <li>
-                Nombre de manches pour gagner: <b>{game.roundsToWin}</b>
+                Nombre de manches pour gagner:{' '}
+                <strong>{game.roundsToWin}</strong>
               </li>
               <li>
-                Temps entre chaque carte: <b>{game.cardSpeed}</b> secondes
+                Temps entre chaque carte: <strong>{game.cardSpeed}</strong>{' '}
+                secondes
               </li>
             </ul>
           )}
 
-          <div className='PlaisGameHost(game, player.id)yers'>
+          <div className='Players'>
             {game.players.map(player => (
               <PlayerBox
                 key={player.id}
