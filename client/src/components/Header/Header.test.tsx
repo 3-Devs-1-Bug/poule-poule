0e1feb291a6cbaeb7342f7445ab304ff7a61dd14
Quentin Bellanger <hello@quentin-bellanger.com>
2020-01-17 22:46:22 +0100

    Fix header tests

--- client/src/components/Header/Header.test.tsx
+++ client/src/components/Header/Header.test.tsx
@@ -4,24 +4,12 @@ import Header from './Header'
 
 describe('Header', () => {
   it('renders without crashing', () => {
-    const { container } = render(<Header title='title' subtitle='subtitle' />)
+    const { container } = render(<Header />)
     expect(container.firstElementChild).toBeInTheDocument()
   })
 
   it('renders a Header', () => {
-    const { container } = render(<Header title='title' subtitle='subtitle' />)
+    const { container } = render(<Header />)
     expect(container.firstElementChild!.tagName).toBe('HEADER')
-  })
-
-  it('renders the title prop', () => {
-    const { getByRole } = render(<Header title='title' subtitle='subtitle' />)
-    const title = getByRole('heading')
-    expect(title).toHaveTextContent('title')
-  })
-
-  it('renders the subtitle prop', () => {
-    const { getByText } = render(<Header title='title' subtitle='subtitle' />)
-    const subtitle = getByText('subtitle')
-    expect(subtitle).toBeDefined()
   })
 })
