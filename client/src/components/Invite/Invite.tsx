3ef3a677b64a5e05beff39256fafca4179eed386
ThunderDev1 <sgranger1@gmail.com>
2020-01-19 14:42:36 +0100

    Improve copy button UX

--- client/src/components/Invite/Invite.tsx
+++ client/src/components/Invite/Invite.tsx
@@ -1,26 +1,27 @@
 import React, { FC } from 'react'
 import CopyToClipboard from 'react-copy-to-clipboard'
 
-import copyIcon from '../../images/copy.png'
 import './Invite.scss'
+import Button from '../Button'
+import useCooldown from '../../hooks/useCooldown'
 
 export interface InviteProps {
   gameUrl: string
 }
 
 const Invite: FC<InviteProps> = ({ gameUrl }) => {
+  const [isCoolingDown, triggerCooldown] = useCooldown(1500)
+
   return (
     <div className='Invite'>
       <h2>Inviter des joueurs</h2>
       <p>Pour inviter des joueurs, partagez le lien ci-dessous:</p>
       <div className='Invite__LinkBlock'>
         <span className='Invite__LinkBlock__Link'>{gameUrl}</span>
-        <CopyToClipboard text={gameUrl}>
-          <img
-            className='Invite__LinkBlock__CopyIcon'
-            src={copyIcon}
-            alt={`copier le lien d'invitaton`}
-          />
+        <CopyToClipboard text={gameUrl} onCopy={triggerCooldown}>
+          <Button disabled={isCoolingDown} className={'Button--Small'}>
+            {isCoolingDown ? 'Copié' : 'Copier'}
+          </Button>
         </CopyToClipboard>
       </div>
     </div>
