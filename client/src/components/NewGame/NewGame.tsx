7f551b7b03451ba32e5ecb195d4c0ce3075ddbee
Quentin Bellanger <hello@quentin-bellanger.com>
2020-01-19 14:43:04 +0100

    Create NewGame component

---
+++ client/src/components/NewGame/NewGame.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react'
+import classnames from 'classnames'
+
+import './NewGame.scss'
+
+import Button from '../Button/Button'
+
+export interface NewGameProps {
+  clickHandler: (event: React.MouseEvent<HTMLButtonElement>) => void
+  className?: string
+}
+
+const NewGame: FC<NewGameProps> = ({ clickHandler, className }) => {
+  const classes = classnames(className, 'NewGame')
+  return (
+    <div className={classes}>
+      <h2>Nouvelle partie</h2>
+      <p className='NewGame__Description'>
+        Crée une nouvelle partie avec tes paramètres favoris puis invite tes
+        amis.
+      </p>
+      <Button className='NewGame__Button' onClick={clickHandler}>
+        Créer une nouvelle partie
+      </Button>
+    </div>
+  )
+}
+
+export default NewGame
---
+++ client/src/components/NewGame/index.ts
@@ -0,0 +1,2 @@
+export * from './NewGame'
+export { default } from './NewGame'
