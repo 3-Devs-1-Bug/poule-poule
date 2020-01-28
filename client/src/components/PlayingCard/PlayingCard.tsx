fbbde53728c5cc764866ae675d90882fb876d405
Quentin Bellanger <hello@quentin-bellanger.com>
2020-01-28 19:30:34 +0100

    Add new cards style

--- client/src/components/PlayingCard/PlayingCard.tsx
+++ client/src/components/PlayingCard/PlayingCard.tsx
@@ -3,29 +3,11 @@ import React, { FC } from 'react'
 import './PlayingCard.scss'
 import { CardType } from '../../types/CardType'
 
-const cardToEmoji = (card: CardType) => {
-  let emoji
-  let label
-  switch (card) {
-    case CardType.EGG:
-      emoji = '🥚'
-      label = 'Egg'
-      break
-    case CardType.HEN:
-      emoji = '🐔'
-      label = 'Hen'
-      break
-    case CardType.FOX:
-      emoji = '🦊'
-      label = 'Fox'
-      break
-  }
-  return (
-    <span className='Card__Emoji' role='img' aria-label={label}>
-      {emoji}
-    </span>
-  )
-}
+const cardList: { type: string; emoji: string; label: string }[] = [
+  { type: 'EGG', emoji: '🥚', label: 'Oeuf' },
+  { type: 'HEN', emoji: '🐔', label: 'Poule' },
+  { type: 'FOX', emoji: '🦊', label: 'Renard' }
+]
 
 export interface CardProps {
   type: CardType
@@ -33,8 +15,33 @@ export interface CardProps {
 }
 
 const Card: FC<CardProps> = ({ type, className }) => {
-  const classes = 'Card__Container ' + className
-  return <div className={classes}>{cardToEmoji(type)}</div>
+  const classes = 'Card ' + className
+  const currentCard = cardList.find(card => card.type === type)
+  return (
+    <div className={classes} aria-label={currentCard && currentCard.label}>
+      <div className='Card__Stripe Card__Stripe--Left'>
+        {[...Array(11)].map((_, i) => {
+          return <div className='Card__Stripe--Hole' key={i} />
+        })}
+      </div>
+
+      <div className='Card__Content'>
+        {[...Array(3)].map((_, i) => {
+          return (
+            <span role='img' key={i}>
+              {currentCard && currentCard.emoji}
+            </span>
+          )
+        })}
+      </div>
+
+      <div className='Card__Stripe Card__Stripe--Right'>
+        {[...Array(11)].map((_, i) => {
+          return <div className='Card__Stripe--Hole' key={i} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Card
