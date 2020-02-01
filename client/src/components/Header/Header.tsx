aa77d4263a5b89ab2c1056c5d357e14a1f22981f
Quentin Bellanger <hello@quentin-bellanger.com>
2020-02-01 17:48:14 +0100

    Add logo in src/ folder

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import classnames from 'classnames'
 
+import logo from '../../images/logo.png'
 import './Header.scss'
 import { Link } from '@reach/router'
 
@@ -14,7 +15,7 @@ const Header: FC<HeaderProps> = ({ className }) => {
   return (
     <header className={classes}>
       <Link className='Header__Link' to='/'>
-        <img className='Header__Icon' src='/icon-512x512.png' alt='' />
+        <img className='Header__Icon' src={logo} alt='' />
         <h1>Poule Poule</h1>
       </Link>
     </header>
