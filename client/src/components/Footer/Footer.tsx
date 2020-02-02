c31f8aa1306cef375f6e142a7502cdfd5ebded78
Quentin Bellanger <hello@quentin-bellanger.com>
2020-02-02 15:03:36 +0100

    Import external component

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from 'react'
 import classnames from 'classnames'
-
-import './Footer.scss'
 import { Link } from '@reach/router'
+
+import External from '../../components/External'
+import './Footer.scss'
 
 export interface FooterProps {
   className?: string
@@ -29,14 +30,7 @@ const Footer: FC<FooterProps> = ({ className }) => {
             rel='noopener noreferrer'
           >
             Acheter le jeu
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='currentColor'
-              viewBox='0 0 24 24'
-            >
-              <title>(ouvre dans un nouvel onglet)</title>
-              <path d='M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z' />
-            </svg>
+            <External />
           </a>
         </li>
       </ul>
