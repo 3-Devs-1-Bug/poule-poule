a4a0df6b352007f218a7bd0c5a538cacbaa6452f
Adrien BOUTIGNY <adrien.boutigny@gmail.com>
2020-02-02 13:35:13 +0100

    add Countdown style

--- client/src/pages/Demo.tsx
+++ client/src/pages/Demo.tsx
@@ -1,20 +1,30 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, useState } from 'react'
 import { RouteComponentProps } from '@reach/router'
 
 import Countdown from '../components/Countdown'
+import Button from '../components/Button'
 
 export interface DemoProps extends RouteComponentProps {}
 
 const Demo: FC<DemoProps> = () => {
   const [count, setCount] = useState(10)
+  const [countdownStarted, setCountdownStarted] = useState(false)
 
-  useEffect(() => {
+  const startCountdown = () => {
     setInterval(() => {
       setCount(current => current - 1)
     }, 1000)
-  }, [])
+    setCountdownStarted(true)
+  }
 
-  return <Countdown count={count} />
+  return (
+    <>
+      <Button onClick={startCountdown} disabled={countdownStarted}>
+        Start countdown
+      </Button>
+      {countdownStarted && <Countdown count={count} />}
+    </>
+  )
 }
 
 export default Demo
